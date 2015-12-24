b90a904f0fd10b4024935974479b4cf670bc390a
econo <mr.sharpen@foxmail.com>
2015-12-24 18:04:54 +0800

    comment: todo search in rotated sorted array

--- search_in_rotated_sorted_array.py
+++ search_in_rotated_sorted_array.py
@@ -21,7 +21,24 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        first_index = 0
+        last_index = len(nums) - 1
+        mid_index = int(last_index / 2)
+        while first_index < last_index:
+            mid = nums[mid_index]
+            if target == mid:
+                return mid_index
+            elif target > mid:
+                first = nums[first_index]
+                last = nums[last_index]
+                if last > mid:
+                    pass
+                else:
+                    pass
 
+            elif target < mid:
+                pass
+        return -1
 
 solution = Solution()
 print(solution.search([3, 4, 5, 6, 1, 2], 5))
